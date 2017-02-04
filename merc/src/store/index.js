620aabf99621cb3224e8eb2fdb061d087f5232b2
Rusty Fausak <rustyfausak@gmail.com>
2017-02-03 18:00:34 -0600

    add parameterization to stats/summary call

--- merc/src/store/index.js
+++ merc/src/store/index.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { getResource } from './api'
+var _ = require('lodash')
 
 Vue.use(Vuex)
+
+function getQueryString (params) {
+  params = params || {}
+  if (!_.size(params)) {
+    return ''
+  }
+  let qs = '?'
+  let i = 0
+  for (let key in params) {
+    if (i > 0) {
+      qs += '&'
+    }
+    qs += key + '=' + encodeURIComponent(params[key])
+    i++
+  }
+  return qs
+}
 
 const store = new Vuex.Store({
   state: {
     cache: {}
   },
   actions: {
-    GET_STATS_SUMMARY: function ({ dispatch }) {
-      return dispatch('FETCH', { endpoint: 'stats/summary' })
+    GET_STATS_SUMMARY: function ({ dispatch }, params) {
+      let endpoint = 'stats/summary' + getQueryString(params)
+      return dispatch('FETCH', { endpoint })
     },
     FETCH: function ({ commit, state }, { endpoint }) {
       if (endpoint in state.cache) {
