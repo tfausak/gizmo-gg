825aa16ca1807c1e5ee72abe7b6b9bbf98736d6c
Rusty Fausak <rustyfausak@gmail.com>
2017-02-09 01:29:58 -0600

    api delay to config

--- merc/config/dev.env.js
+++ merc/config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: process.env.API_URL || '"http://127.0.0.1:8080/"'
+  API_URL: process.env.API_URL || '"http://127.0.0.1:8080/"',
+  API_DELAY_MS: 1000
 })
