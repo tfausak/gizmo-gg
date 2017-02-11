38980d2eded435059e9c5dd2412126f5b51112e9
Taylor Fausak <taylor@fausak.me>
2017-02-10 19:11:20 -0600

    Simplify test suite

--- paladin/source/test/Main.hs
+++ paladin/source/test/Main.hs
@@ -1,12 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
+module Main
+  ( main
+  ) where
 
-module Main where
-
-import Data.Function ((&))
-
-import qualified Data.Aeson as Aeson
-import qualified Data.ByteString as ByteString
-import qualified Network.Wai as Wai
+import qualified Data.ByteString.Char8 as ByteString
 import qualified Network.Wai.Test as Test
 import qualified Paladin
 
@@ -21,74 +17,28 @@ main = do
 
 session :: Test.Session ()
 session = do
-  do let request = Test.defaultRequest
-     response <- Test.request request
-     assertJson 200 Aeson.Null response
-  do let request = Test.defaultRequest & setPath "/not-found"
-     response <- Test.request request
-     assertJson 404 Aeson.Null response
-  do let request = Test.defaultRequest & setPath "/arenas"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Arena]) response
-  do let request = Test.defaultRequest & setPath "/game-modes"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.GameMode]) response
-  do let request = Test.defaultRequest & setPath "/game-types"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.GameType]) response
-  do let request = Test.defaultRequest & setPath "/games-players"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.GamePlayer]) response
-  do let request = Test.defaultRequest & setPath "/games"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Game]) response
-  do let request = Test.defaultRequest & setPath "/parse-errors"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.ParseError]) response
-  do let request = Test.defaultRequest & setPath "/parsers"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Parser]) response
-  do let request = Test.defaultRequest & setPath "/platforms"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Platform]) response
-  do let request = Test.defaultRequest & setPath "/players"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Player]) response
-  do let request = Test.defaultRequest & setPath "/playlists"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Playlist]) response
-  do let request = Test.defaultRequest & setPath "/replays"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Replay]) response
-  do let request = Test.defaultRequest & setPath "/servers"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Server]) response
-  do let request = Test.defaultRequest & setPath "/stats/summary"
-     response <- Test.request request
-     let json =
-           Aeson.object
-             [ ("body_freq_pct", Aeson.object [])
-             , ("map_freq_pct", Aeson.object [])
-             , ( "win_pct"
-               , Aeson.object
-                   [("blue", Aeson.Number 0), ("orange", Aeson.Number 0)])
-             ]
-     assertJson 200 json response
-  do let request = Test.defaultRequest & setPath "/uploads"
-     response <- Test.request request
-     assertJson 200 ([] :: [Paladin.Upload]) response
-  do let request = Test.defaultRequest & setPath "/uploads/invalid-id"
-     response <- Test.request request
-     assertJson 400 Aeson.Null response
+  assertStatus 200 "/"
+  assertStatus 404 "/not-found"
+  assertStatus 200 "/arenas"
+  assertStatus 200 "/game-modes"
+  assertStatus 200 "/game-types"
+  assertStatus 200 "/games-players"
+  assertStatus 200 "/games"
+  assertStatus 200 "/parse-errors"
+  assertStatus 200 "/parsers"
+  assertStatus 200 "/platforms"
+  assertStatus 200 "/players"
+  assertStatus 200 "/playlists"
+  assertStatus 200 "/replays"
+  assertStatus 200 "/servers"
+  assertStatus 200 "/stats/summary"
+  assertStatus 200 "/uploads"
+  assertStatus 400 "/uploads/invalid-id"
+  assertStatus 404 "/uploads/0"
 
-assertJson
-  :: Aeson.ToJSON json
-  => Int -> json -> Test.SResponse -> Test.Session ()
-assertJson status json response = do
+assertStatus :: Int -> String -> Test.Session ()
+assertStatus status rawPath = do
+  let path = ByteString.pack rawPath
+  let request = Test.setPath Test.defaultRequest path
+  response <- Test.request request
   Test.assertStatus status response
-  Test.assertContentType "application/json" response
-  let body = Aeson.encode json
-  Test.assertBody body response
-
-setPath :: ByteString.ByteString -> Wai.Request -> Wai.Request
-setPath path request = Test.setPath request path
