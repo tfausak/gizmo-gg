378cad90c45e4d2953583ca6812e1d190afbc209
Taylor Fausak <taylor@fausak.me>
2017-02-25 09:53:38 -0600

    Point to the latest release

--- merc/src/pages/DesktopApp.vue
+++ merc/src/pages/DesktopApp.vue
@@ -9,7 +9,7 @@
           <h2 class="subtitle">
             Automatically upload your saved replays to gizmo.gg
           </h2>
-          <a class="button is-white-outlined is-medium" target="_blank" href="https://github.com/tfausak/gizmo-gg-uploader/releases">
+          <a class="button is-white-outlined is-medium" target="_blank" href="https://github.com/tfausak/gizmo-gg-uploader/releases/latest">
             <span class="icon">
               <i class="fa fa-download"></i>
             </span>
