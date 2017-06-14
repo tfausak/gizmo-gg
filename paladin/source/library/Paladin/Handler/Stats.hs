126f736abad847319b716ba30e850de89e15a2bd
Taylor Fausak <taylor@fausak.me>
2017-06-13 21:31:04 -0500

    Only return 10 games at a time

--- paladin/source/library/Paladin/Handler/Stats.hs
+++ paladin/source/library/Paladin/Handler/Stats.hs
@@ -683,7 +683,7 @@ getGames connection day playlists templates after player =
         arena_templates.name IN ? AND
         games_players.player_id = ?
       ORDER BY games.played_at DESC
-      LIMIT 20
+      LIMIT 10
     |]
     (day, after, Common.In playlists, Common.In templates, player)
 
